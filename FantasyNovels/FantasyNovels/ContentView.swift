d9c634b12dfe5391d042b0f462722f4a0b947821
Sergey Razgulyaev <razgulyaev.sergey@gmail.com>
2024-11-30 15:30:18 +0300

    [RSN-001] Реализовать навигацию в МП FantasyNovels

---
+++ FantasyNovels/FantasyNovels/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FantasyNovels
+//
+//  Created by Sergey Razgulyaev on 30.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FantasyNovels/FantasyNovels/FantasyNovelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantasyNovelsApp.swift
+//  FantasyNovels
+//
+//  Created by Sergey Razgulyaev on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct FantasyNovelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
