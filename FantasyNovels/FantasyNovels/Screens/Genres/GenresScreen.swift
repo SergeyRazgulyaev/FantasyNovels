908d7e8d1a03d00c9dda6ae641c7f609b084991c
Sergey Razgulyaev <razgulyaev.sergey@gmail.com>
2024-12-01 17:25:18 +0300

    [RSN-001] Реализовать витрину

--- FantasyNovels/FantasyNovels/Screens/Genres/GenresScreen.swift
+++ FantasyNovels/FantasyNovels/Screens/Genres/GenresScreen.swift
@@ -13,25 +13,26 @@ struct GenresScreen: View {
     // MARK: - Constants
 
     private enum Constants {
-        static let genresTitle: String = "Жанры\nфантастической литературы"
-        static let firstGenreTitle: String = "Лучшие зарубежные авторы"
-        static let secondGenreTitle: String = "Лучшие российские авторы"
-        static let thirdGenreTitle: String = "Вся мировая фантастика"
+        static let genresTitle: String = "Жанры"
+        static let foreignGenreTitle: String = "Лучшие зарубежные авторы"
+        static let russianGenreTitle: String = "Лучшие российские авторы"
+        static let worldGenreTitle: String = "Все авторы"
 
-        static let firstGenreDescription: String =  "Читай лучших зарубежных авторов мировой фантастики!"
-        static let secondGenreDescription: String =  "Читай лучших российских авторов мировой фантастики!"
-        static let thirdGenreDescription: String =  "Все произведения мировой фантастики"
+        static let foreignGenreDescription: String =  "Читай произведения лучших зарубежных авторов мировой фантастики!"
+        static let russianGenreDescription: String =  "Читай произведения лучших российских авторов мировой фантастики!"
+        static let worldGenreDescription: String =  "Все произведения мировой фантастики"
 
-        static let genreImageSystemName: String = "books.vertical.fill"
+        static let foreignGenreImageName: String = "foreign_fantasy"
+        static let russianGenreImageName: String = "russian_fantasy"
+        static let worldGenreImageName: String = "world_fantasy"
 
-        static let firstGenreImageForegroundColor: Color = .blue
-        static let secondGenreImageForegroundColor: Color = .red
-        static let thirdGenreImageForegroundColor: Color = .green
+        static let foreignGenreImageForegroundColor: Color = .blue
+        static let russianGenreImageForegroundColor: Color = .red
+        static let worldGenreImageForegroundColor: Color = .green
         static let descriptionForegroundColor: Color = .gray
 
         static let hStackSpacing: CGFloat = 10.0
-        static let genreImageWidth: CGFloat = 30.0
-        static let genreImageHeight: CGFloat = 30.0
+        static let genreImageCornerRadius: CGFloat = 10.0
         static let genrePadding: CGFloat = 5.0
     }
 
@@ -39,57 +40,45 @@ struct GenresScreen: View {
 
     /// Жанры фантастической литературы
     private let genres: [GenresCellModel] = [
-        .init(title: Constants.firstGenreTitle,
-              description: Constants.firstGenreDescription,
+        .init(title: Constants.foreignGenreTitle,
+              description: Constants.foreignGenreDescription,
               descriptionTextColor: Constants.descriptionForegroundColor,
-              imageName: Constants.genreImageSystemName,
-              imageForegroundColor: Constants.firstGenreImageForegroundColor),
-        .init(title: Constants.secondGenreTitle,
-              description: Constants.secondGenreDescription,
+              imageName: Constants.foreignGenreImageName,
+              imageForegroundColor: Constants.foreignGenreImageForegroundColor),
+        .init(title: Constants.russianGenreTitle,
+              description: Constants.russianGenreDescription,
               descriptionTextColor: Constants.descriptionForegroundColor,
-              imageName: Constants.genreImageSystemName,
-              imageForegroundColor: Constants.secondGenreImageForegroundColor),
-        .init(title: Constants.thirdGenreTitle,
-              description: Constants.thirdGenreDescription,
+              imageName: Constants.russianGenreImageName,
+              imageForegroundColor: Constants.russianGenreImageForegroundColor),
+        .init(title: Constants.worldGenreTitle,
+              description: Constants.worldGenreDescription,
               descriptionTextColor: Constants.descriptionForegroundColor,
-              imageName: Constants.genreImageSystemName,
-              imageForegroundColor: Constants.thirdGenreImageForegroundColor)
+              imageName: Constants.worldGenreImageName,
+              imageForegroundColor: Constants.worldGenreImageForegroundColor)
     ]
 
     var body: some View {
-
         NavigationView {
-
-            VStack {
-                Text(Constants.genresTitle)
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-
-                List(genres) { item in
-                    NavigationLink(destination: Text(item.title)) {
-                        VStack(alignment: .leading) {
-                            HStack(spacing: Constants.hStackSpacing) {
-                                Image(systemName: item.imageName)
-                                    .resizable()
-                                    .frame(width: Constants.genreImageWidth,
-                                           height: Constants.genreImageHeight)
-                                    .foregroundColor(item.imageForegroundColor)
-                                Text(item.title)
-                                    .font(.headline)
-                                    .fontWeight(.medium)
-                                    .multilineTextAlignment(.leading)
-                            }
+            List(genres) { item in
+                NavigationLink(destination: Text(item.title)) {
+                    VStack(alignment: .leading) {
+                        Text(item.title)
+                            .font(.headline)
+                            .fontWeight(.medium)
+                            .multilineTextAlignment(.leading)
+                        Image(item.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(item.imageForegroundColor)
+                            .cornerRadius(Constants.genreImageCornerRadius)
                             .padding(.all, Constants.genrePadding)
-
-                            Text(item.description)
-                                .foregroundColor(item.descriptionTextColor)
-                                .multilineTextAlignment(.leading)
-                                .padding([.leading, .bottom, .trailing], Constants.genrePadding)
-                        }
+                        Text(item.description)
+                            .foregroundColor(item.descriptionTextColor)
+                            .multilineTextAlignment(.leading)
+                            .padding([.leading, .bottom, .trailing], Constants.genrePadding)
                     }
                 }
-            }
+            }.navigationBarTitle(Constants.genresTitle)
         }
     }
 }
--- FantasyNovels/FantasyNovels/Screens/Showcase/ShowcaseScreen.swift
+++ FantasyNovels/FantasyNovels/Screens/Showcase/ShowcaseScreen.swift
@@ -13,14 +13,41 @@ struct ShowcaseScreen: View {
     // MARK: - Constants
 
     private enum Constants {
+        static let showcaseTitle: String = "Зарубежная и российская фантастика"
+        static let showcaseImageName: String = "space_ship"
         static let readButtonLabel: String = "Читать"
+
+        static let readButtonTextColor: Color = .black
+
+        static let showcaseVStackSpacing: CGFloat = 5.0
+        static let showcaseTitleVerticalPadding: CGFloat = 10.0
+        static let buttonVerticalPadding: CGFloat = 30.0
     }
 
     // MARK: - Properties
 
     var body: some View {
-        Button(Constants.readButtonLabel) {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+        VStack(alignment: .center) {
+
+            VStack(alignment: .center,
+                   spacing: Constants.showcaseVStackSpacing) {
+                Text(Constants.showcaseTitle)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, Constants.showcaseTitleVerticalPadding)
+                Image(Constants.showcaseImageName,
+                      label: Text(Constants.showcaseTitle))
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            }
+
+            Button(action: {
+                print("Action")
+            }) {
+                Text(Constants.readButtonLabel)
+                    .foregroundColor(Constants.readButtonTextColor)
+            }.padding(.vertical, Constants.buttonVerticalPadding)
         }
     }
 }
